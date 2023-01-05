4bb0489da6825b68caef5f13e8abfb306969db92
ParasRawat29 <parasrawat29pr@gmail.com>
2023-01-06 00:45:02 +0530

    dashboard css changed

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,7 +1,23 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import Badge from "../../components/badge";
 import useEventSource from "../../custom hooks/useEventSource";
 import style from "./style.module.scss";
+
+const COLOR = {
+  closed: {
+    bgColor: "var(--success)",
+    textColor: "white",
+  },
+  investigating: {
+    bgColor: "#3970c9",
+    textColor: "white",
+  },
+  reported: {
+    bgColor: "var(--caution)",
+    textColor: "white",
+  },
+};
 
 function Dashboard() {
   const navigate = useNavigate();
@@ -15,32 +31,56 @@ function Dashboard() {
 
   if (!data) return <p>loading</p>;
   if (data.length === 0) return <p>No Reports Yet</p>;
+
   return (
     <div className={style["dashboardWrapper"]}>
-      <table className={style["table"]}>
-        <thead>
-          <tr>
-            <th>S.No.</th>
-            <th>Product</th>
-            <th>Reported by</th>
-            <th>Reporting Date</th>
-            <th>Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((item) => {
-            return (
-              <tr onClick={() => handleReportClick(item._id)}>
-                <td>1</td>
-                <td>{item.productId.title}</td>
-                <td>{item.reportedUser.name}</td>
-                <td>{new Date(item.reportingDate).toLocaleDateString()}</td>
-                <td>{item.status}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <div className={style["tableContainer"]}>
+        <table className={style["table"]}>
+          <thead>
+            <tr>
+              <th>S.No.</th>
+              <th>Product</th>
+              <th>Reported by</th>
+              <th>Reporting Date</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, idx) => {
+              return (
+                <tr
+                  onClick={() => handleReportClick(item._id)}
+                  className={item.status === "closed" ? style["closed"] : ""}
+                >
+                  <td>{idx + 1}</td>
+                  <td>{item.productId.title}</td>
+                  <td className={style["userField"]}>
+                    <span>{item.reportedUser.name}</span>
+                    {item.reportedUser.profile &&
+                      item.reportedUser.profile !== "" && (
+                        <img
+                          src={item.reportedUser.profile}
+                          alt="profilepic"
+                          width="30px"
+                          height="30px"
+                        />
+                      )}
+                  </td>
+
+                  <td>{new Date(item.reportingDate).toLocaleDateString()}</td>
+                  <td>
+                    <Badge
+                      text={item.status}
+                      textColor={COLOR[item.status].textColor}
+                      bgColor={COLOR[item.status].bgColor}
+                    />
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
